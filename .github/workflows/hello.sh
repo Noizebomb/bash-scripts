a95f49f4457149954b743381855ea77e6957af42
Noizebomb <73506881+Noizebomb@users.noreply.github.com>
2021-01-11 13:51:39 -0500

    Delete hello.sh

--- .github/workflows/hello.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-greeting="welcome back"
-user=$(whoami)
-day=$(date +%A)
-
-echo "$greeting $user! Today is $day, which is the best day of the week!"
-echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
